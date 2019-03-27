7ba33315dd76836e27eb4f8173e0564f67623c0d
Pietro Dellino <pietro.dellino@student.ecp.fr>
2019-03-27 14:41:18 +0100

    Network Test

--- Bicycle_G2/src/controller/NetworkManagerTest.java
+++ Bicycle_G2/src/controller/NetworkManagerTest.java
@@ -2,15 +2,22 @@ package controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class NetworkManagerTest {
+import ride.Network;
+import station.Station;
 
+class NetworkManagerTest {
+	
+	/*
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
-
+	*/
+	
 	@Test
 	void testNetworkManager() {
 		NetworkManager nm = new NetworkManager();
@@ -19,8 +26,34 @@ class NetworkManagerTest {
 
 	@Test
 	void testSetupNetworkString() {
-		
-		fail("Not yet implemented");
+		NetworkManager nm = new NetworkManager();
+		assertAll(
+			() -> {
+				System.out.println("bonjout");
+				Network net = nm.setupNetwork("VLib");
+				System.out.println("bonjout");
+				ArrayList<Station> stations = net.getStations();
+				assertAll("Properties of the network",
+					() -> assertEquals(10, net.getStations().size()),
+					() -> {
+						for (int i = 0; i < stations.size(); i++) {
+							assertEquals(10, stations.get(i).getParkingSlots().size(), "Number of slots of station number " + i);
+						}
+					},
+					() -> {
+						for (int i = 0; i < stations.size(); i++) {
+							Station s = stations.get(i);
+							String is = ((Integer) i).toString();
+							assertAll(
+								() -> assertTrue(0 <= s.getP().getX() && s.getP().getX() < 4, "X position of station number " + is),
+								() -> assertTrue(0 <= s.getP().getY() && s.getP().getY() < 4, "Y position of station number " + is)
+							);
+						}
+					}
+				);
+			},
+			() -> assertThrows(ExistingNameException.class, () -> { Network net1 = nm.setupNetwork("VLib"); })
+		);
 	}
 
 	@Test
---
+++ Bicycle_G2/src/ride/test/NetworkTest.java
@@ -0,0 +1,64 @@
+package ride.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class NetworkTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testNetwork() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNetworkString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetStations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddStation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveStation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSortingStations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testArchiveRide() {
+		fail("Not yet implemented");
+	}
+
+}
