a1e8bd627e8bcd9ea5648b88894a97b67babc098
Pietro Dellino <pietro.dellino@student.ecp.fr>
2019-03-28 22:48:31 +0100

    Command Line Interpreter started

--- Bicycle_G2/src/ui/clui/CommandLineInterpreter.java
+++ Bicycle_G2/src/ui/clui/CommandLineInterpreter.java
@@ -2,6 +2,10 @@ package ui.clui;
 
 public class CommandLineInterpreter {
 	
+	private String[] commands= {"setup", "runtest", "setStationOnline", "setStationOffline", "rentBike", "returnBike", "addBike", "addStation", "addSlot", "setSlotOnline", "setSlotOffline"};
 	
+	public CommandLineInterpreter() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
