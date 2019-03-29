423023e3d93c59b7671c21e5c9ce6d0c480666e6
Pietro Dellino <pietro.dellino@student.ecp.fr>
2019-03-29 17:34:24 +0100

    Command Line

--- Bicycle_G2/src/ui/clui/CommandLineInterpreter.java
+++ Bicycle_G2/src/ui/clui/CommandLineInterpreter.java
@@ -1,12 +1,35 @@
 package ui.clui;
 
+import java.util.Scanner;
+
 public class CommandLineInterpreter {
-	
-	private String[] commands = {"setup", "runtest", "setStationOnline", "setStationOffline", "rentBike", "returnBike", "addBike", "addStation", "addSlot", "setSlotOnline", "setSlotOffline", "calculateItinerary"};
-	
-	public CommandLineInterpreter() {
-		// TODO Auto-generated constructor stub
 		
+	public CommandLineInterpreter() {}
+	
+	public String readCommand() {
+		Scanner sc = new Scanner(System.in);
+		String command = sc.nextLine();
+		sc.close();
+		return command;
+	}
+	
+	public String readCommand(String s) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(s);
+		String command = sc.nextLine();
+		sc.close();
+		return command;
+	}
+	
+	public Object[] parseCommand(String s) {
+		String[] commandLine = s.split(" ");
+		String command = commandLine[0];
+		for (Command c : Command.values()) {
+			if (command.equals(c.getKeyword())) {
+				return c;
+			}
+		}
+		return null;
 	}
 
 }
