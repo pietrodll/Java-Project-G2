28a8f86db90fa1230aae0df47a8fc96f38ce008e
Pietro Dellino <pietro.dellino@student.ecp.fr>
2019-03-31 17:42:46 +0200

    added format

--- Bicycle_G2/src/ui/clui/Command.java
+++ Bicycle_G2/src/ui/clui/Command.java
@@ -2,31 +2,34 @@ package ui.clui;
 
 public enum Command {
 	
-	SETUP("setup"),
-	RUNTEST("runtest"),
-	STATION_ONLINE("setStationOnline"),
-	STATION_OFFLINE("setStationOffline"),
-	SLOT_ONLINE("setSlotOnline"),
-	SLOT_OFFLINE("setSlotOffline"),
-	ADD_STATION("addStation"),
-	ADD_SLOT("addSlot"),
-	ADD_BIKE("addBike"),
-	ADD_USER("addUser"),
-	RETURN_BIKE("returnBike"),
-	RENT_BIKE("rentBike"),
-	DISPLAY_USER("displayUser"),
-	DISPLAY_STATION("displayStation"),
-	DISPLAY("display"),
-	SORT_STATION("sortStation"),
-	CALCULATE_ITINERARY("calculateItinerary");
+	SETUP("setup", "setup <networkName> or setup <networkName> <nStations> <nSlots> <side> <nBikes>"),
+	RUNTEST("runtest", "runtest <fileName>"),
+	STATION_ONLINE("setStationOnline", "stationOnline <networkName> <stationID>"),
+	STATION_OFFLINE("setStationOffline", "stationOffline <networkName> <stationID>"),
+	SLOT_ONLINE("setSlotOnline", "slotOnline <networkName> <slotID> <time>"),
+	SLOT_OFFLINE("setSlotOffline", "slotOffline <networkName> <slotID> <time>"),
+	ADD_STATION("addStation", "addStation <networkName> <type> <numSlots> <positionX> <positionY>"),
+	ADD_SLOT("addSlot", "addSlot <networkName> <stationID> <numSlots>"),
+	ADD_BIKE("addBike", "addBike <networkName> <type> <time>"),
+	ADD_USER("addUser", "addUser <username> <cardType> <networkName>"),
+	RETURN_BIKE("returnBike", "returnBike <userID> <stationID> <time> <networkName>"),
+	RENT_BIKE("rentBike", "rentBike <userID> <stationID> <bikeType> <time> <networkName>"),
+	DISPLAY_USER("displayUser", "displayUser <networkName> <userID>"),
+	DISPLAY_STATION("displayStation", "displayStation <networkName> <stationID>"),
+	DISPLAY("display", "display <networkName>"),
+	SORT_STATION("sortStation", "sortStation <networkName> <more-used> or sortStation <networkName> <least-occupied> <startTime> <endTime>"),
+	CALCULATE_ITINERARY("calculateItinerary", "calculateItinerary <networkName> <userID> <startX> <startY> <destinationX> <destinationY> <pathStrategy>");
 	
 	
 	private String keyword;
+	private String commandFormat;
 	
-	Command(String s) {
+	Command(String s, String t) {
 		this.keyword = s;
+		this.commandFormat = t;
 	}
 	
 	public String getKeyword() { return this.keyword; }
+	public String getFormat() { return this.commandFormat; }
 
 }
